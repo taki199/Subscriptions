727a471220d7b2429029da0b5665ac1493510f89
HOUCINE TAKI <christotaki22@gmail.com>
2025-02-13 16:49:54 +0100

    user & subscription model

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "User email is required"],
+      unique: true,
+      trim: true,
+      minLength: 5,
+      maxLength: 255,
+      lowerCase: true,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
+    },
+    poassword: {
+      type: String,
+      required: [true, "User password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
