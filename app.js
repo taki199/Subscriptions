593b2bf6cf7f234e051ed1f506c2b3a107cd33be
HOUCINE TAKI <christotaki22@gmail.com>
2025-02-14 16:04:04 +0100

    middleware

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import userRouter from "./routes/user.routes.js";
 
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 // api/v1/auth/sign-up
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("welcome to the subscription tracker api");
