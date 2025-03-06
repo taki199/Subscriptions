81fd2f253092f4a5ebf5db8fbd2035111195ea6b
HOUCINE TAKI <christotaki22@gmail.com>
2025-03-06 17:06:29 +0000

    authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "get all users" }));
-userRouter.get("/:id", (req, res) => res.send({ title: "get user details" }));
+userRouter.get("/", getUsers);
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/", (req, res) => res.send({ title: "creat new user" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "update user" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "delete user" }));
